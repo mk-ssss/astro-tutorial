26eec21c6015b19c0dc0314ddb1ad81458407944
poyopuyoyukidaruma <sawayama.mk@gmail.com>
2025-07-16 18:00:34 +0900

    チュートリアルクリア

--- tutorial/astro.config.mjs
+++ tutorial/astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+      build: {
+          rollupOptions: {
+              // external: ["@astrojs/rss"],
+          },
+      },
+  },
+
+  integrations: [preact()],
+});
